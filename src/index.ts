51140faf8e715c80857dff8039610e3a676e821f
jaw <2135326728@qq.com>
2023-04-07 18:01:15 +0800

    feat: init project

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,49 @@
-import { window } from 'vscode'
+import path from 'node:path'
+import type { ExtensionContext } from 'vscode'
+import { StatusBarAlignment, commands, window, workspace } from 'vscode'
+import rimraf from 'rimraf'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+async function getRunItem() {
+  const folders = workspace.workspaceFolders
+
+  if (folders) {
+    if (folders.length > 1) {
+      const projects = folders.map(el => ({ path: el.uri.fsPath, label: el.name }))
+      const select = await window.showQuickPick(projects, { placeHolder: 'Select the project you want to start' })
+      if (select)
+        return select
+      else
+        return Promise.reject(Error('Cancel'))
+    }
+    else {
+      return { path: folders[0].uri.fsPath, label: folders[0].name }
+    }
+  }
+  else {
+    window.showErrorMessage('There are currently no projects')
+    return Promise.reject(Error('There are currently no projects'))
+  }
+}
+
+async function cleanNodeModules() {
+  try {
+    const select = await getRunItem()
+    const targetPath = path.join(select.path, 'node_modules')
+    await rimraf(targetPath, { glob: true })
+    window.showInformationMessage('Clean node_modules Ok')
+  }
+  catch (error) {
+    window.showErrorMessage('Clean node_modules ERROR.Please clean it manually.')
+  }
+}
+
+export function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('extension.cleanNodeModules', cleanNodeModules)
+  context.subscriptions.push(disposable)
+  const bar = window.createStatusBarItem(StatusBarAlignment.Left, 0)
+  bar.text = '$(trashcan) clean node_modules'
+  bar.command = 'extension.cleanNodeModules'
+  bar.show()
 }
 
 export function deactivate() {
